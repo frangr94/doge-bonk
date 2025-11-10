b42c44117125aa1244e39a7d9e503358015eab5a
frangr94 <fgarciarosales10@gmail.com>
2025-11-10 20:50:48 -0300

    improve shadow pixeler

--- scripts/enemies/shadow_pixeler.gd
+++ scripts/enemies/shadow_pixeler.gd
@@ -5,6 +5,7 @@ extends Area2D
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var collision_shape_2d: CollisionShape2D = $killzone/CollisionShape2D
 
 # properties
 var SPEED = 50
@@ -12,21 +13,25 @@ var direction = 1
 var hp = 2
 
 func _on_area_entered(area: Area2D) -> void:
+	# take sword damage
 	if area.is_in_group("sword"):
 		audio_stream_player.play()
 		animated_sprite_2d.play("hit")
+		collision_shape_2d.disabled = true
 		await animated_sprite_2d.animation_finished
 		await audio_stream_player.finished
+		collision_shape_2d.disabled = false
 		hp -= 1
 		animated_sprite_2d.play("run")
 
-
+# take kamehameha damage
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("kamehameha"):
 		audio_stream_player.play()
 		await audio_stream_player.finished
 		hp -= 2
-		
+
+# movement
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
@@ -36,6 +41,7 @@ func _process(delta: float) -> void:
 		animated_sprite_2d.flip_h = true
 	position.x += direction * SPEED * delta
 	
+	# death
 	if hp <= 0:
 		killzone.queue_free()
 		queue_free()
