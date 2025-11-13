e67faf468b45866919d1785713e9c9186177fc26
frangr94 <fgarciarosales10@gmail.com>
2025-11-13 14:34:19 -0300

    minor checkpoint fix

--- scripts/events/checkpoint.gd
+++ scripts/events/checkpoint.gd
@@ -5,3 +5,4 @@ func _on_body_entered(body: Node2D) -> void:
 	print("checkpoint")
 	print(get_tree().current_scene.get_path())
 	SaveLoad.SaveFileData.player_position = global_position
+	SaveLoad._save()
