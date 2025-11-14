e7bbd1debf5f1c535d84c555fa27d7834240011e
frangr94 <fgarciarosales10@gmail.com>
2025-11-14 19:52:30 -0300

    improved old weirdo

--- scripts/events/checkpoint.gd
+++ scripts/events/checkpoint.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("checkpoint")
-	print(get_tree().current_scene.get_path())
+	print("Checkpoint")
 	SaveLoad.SaveFileData.player_position = global_position
 	SaveLoad._save()
