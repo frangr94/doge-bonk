82a6264d173cf6cc2e43db1c91df0f861cf21690
frangr94 <fgarciarosales10@gmail.com>
2025-11-17 15:46:35 -0300

    better bouncer sprite

--- scripts/terrain/hit_bouncer.gd
+++ scripts/terrain/hit_bouncer.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _on_area_entered(area: Area2D):
 	if area.is_in_group("sword") and GameManager.bounce_unlock == true:
+		
 		var player = area.get_parent() as CharacterBody2D
 		if player and player.has_method("bounce"):
+			animated_sprite_2d.play("hit")
 			player.bounce()
 			if GameManager.jump_count >= 1:
 				GameManager.jump_count -= 1
