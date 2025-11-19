aab2e43e67d8cad9b93693c9ecd7657e82433c76
frangr94 <fgarciarosales10@gmail.com>
2025-11-19 09:09:49 -0300

    rampaging pixeler fix

--- scripts/enemies/rampaging_pixeler.gd
+++ scripts/enemies/rampaging_pixeler.gd
@@ -9,41 +9,15 @@ extends Area2D
 @onready var slash_explosion_pixeler: CPUParticles2D = $slash_explosion_pixeler
 
 # properties
-
-var SPEED = EnemySpeedController.rampaging_pixeler
 var direction = 1
-var hp = 1
-
-
-func _on_area_entered(area: Area2D) -> void:
-	# take sword damage
-	if area.is_in_group("sword"):
-		audio_stream_player.play()
-		animated_sprite_2d.play("hit")
-		slash_explosion_pixeler.emitting = true
-		collision_shape_2d.set_deferred("disabled", true)
-		await animated_sprite_2d.animation_finished
-		await audio_stream_player.finished
-		collision_shape_2d.set_deferred("disabled", false)
-		hp -= 1
-		#animated_sprite_2d.play("run")
-
-# take kamehameha damage
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("kamehameha"):
-		audio_stream_player.play()
-		await audio_stream_player.finished
-		hp -= 2
-
+var hp = 2
 # movement
 func _physics_process(delta: float) -> void:
 	var SPEED = EnemySpeedController.rampaging_pixeler
 	if ray_cast_up.is_colliding():
 		direction = 1
-		#animated_sprite_2d.flip_h = false
 	if ray_cast_down.is_colliding():
 		direction = -1
-		#animated_sprite_2d.flip_h = true
 	position.y += direction * SPEED * delta
 	
 	# death
